0263f2387a59c1e2ab749627a09f2e3e72df88c9
vincenter <vincenterr@yahoo.com.tw>
2025-03-07 20:29:34 +0800

    Modify exercise1.10

--- solutions/exercise1.10.js
+++ solutions/exercise1.10.js
@@ -1,12 +1,16 @@
+import { display } from 'sicp';
+
 function A(x, y) {
   return y === 0 ? 0 : x === 0 ? 2 * y : y === 1 ? 2 : A(x - 1, A(x, y - 1));
 }
 
-// A(1, 10);
+display(A(1, 10));
 // == 1024
-// A(2, 4);
+
+display(A(2, 4));
 // == 65536
-// A(3, 3);
+
+display(A(3, 3));
 // == 65536
 
 function f(n) {
