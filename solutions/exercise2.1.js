eb9e25132aa5068d1e88722caa810f351747d83c
vincenter <vincenterr@yahoo.com.tw>
2025-03-12 01:48:45 +0800

    2.1.1

---
+++ solutions/exercise2.1.js
@@ -0,0 +1,20 @@
+import { display, math_abs, math_sign, pair } from 'sicp';
+
+function gcd(a, b) {
+  return b === 0 ? a : gcd(b, a % b);
+}
+
+function make_rat(n, d) {
+  const g = gcd(n, d);
+
+  return pair(math_sign(n) * math_sign(d) * math_abs(n / g), math_abs(d / g));
+}
+
+display(make_rat(8, 12));
+// [2, 3]
+display(make_rat(-8, 12));
+// [-2, 3]
+display(make_rat(8, -12));
+// [-2, 3]
+display(make_rat(-8, -12));
+// [2, 3]
