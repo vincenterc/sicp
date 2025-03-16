78b220ebcf0036965e0c853b69b73ef77f10b0d8
vincenter <vincenterr@yahoo.com.tw>
2025-03-16 20:26:31 +0800

    Modify exercise2.18

--- solutions/exercise2.18.js
+++ solutions/exercise2.18.js
@@ -1,22 +1,22 @@
 import { append, display_list, head, is_null, list, pair, tail } from 'sicp';
 
 function reverse(items) {
-  return is_null(tail(items))
-    ? items
+  return is_null(items)
+    ? null
     : append(reverse(tail(items)), list(head(items)));
 }
 
 display_list(reverse(list(1, 4, 9, 16, 25)));
 // list(25, 16, 9, 4, 1)
 
-function reverse_iter(items) {
-  function iter(items0, result) {
-    return is_null(items0)
+function reverse_iter(items0) {
+  function iter(items, result) {
+    return is_null(items)
       ? result
-      : iter(tail(items0), pair(head(items0), result));
+      : iter(tail(items), pair(head(items), result));
   }
 
-  return iter(items, null);
+  return iter(items0, null);
 }
 
 display_list(reverse_iter(list(1, 4, 9, 16, 25)));
