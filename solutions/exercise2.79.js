8c9535f1b4299662972933a4c4563606ca6cb16e
vincenter <vincenterr@yahoo.com.tw>
2025-04-07 09:45:01 +0800

    Modify exercise 2.79 and 2.80

--- solutions/exercise2.79.js
+++ solutions/exercise2.79.js
@@ -2,21 +2,21 @@ function is_equal(x, y) {
   return apply_generic("is_equal", list(x, y));
 }
 
-// add this to the javascript umber package:
+// to be included in the javascript umber package
 put(
   "is_equal",
   list("javascript_number", "javascript_number"),
   (x, y) => x === y
 );
 
-// add this to the rational package:
+// to be included in the rational package
 put(
   "is_equal",
   list("rational", "rational"),
   (x, y) => numer(x) * demon(y) === denom(x) * numer(y)
 );
 
-// add this to the complex package:
+// to be included in the complex package
 put(
   "is_equal",
   list("complex", "complex"),
--- solutions/exercise2.80.js
+++ solutions/exercise2.80.js
@@ -2,13 +2,13 @@ function is_equal_to_zero(x) {
   return apply_genetic("is_equal_to_zero", list(x));
 }
 
-// add this to the javascript umber package:
+// to be included in the javascript umber package
 put("is_equal_to_zero", list("javascript_number"), (x) => x === 0);
 
-// add this to the rational package:
+// to be included in the rational package
 put("is_equal_to_zero", list("rational"), (x) => numer(x) === 0);
 
-// add this to the complex package:
+// to be included in the complex package
 put(
   "is_equal_to_zero",
   list("complex"),
