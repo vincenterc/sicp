36b54fc9d8d52db06742f1de52f498cecf47dda5
vincenter <vincenterr@yahoo.com.tw>
2025-05-14 08:30:38 +0800

    Modify exercise 2.66

--- solutions/exercise2.66.js
+++ solutions/exercise2.66.js
@@ -1,10 +1,17 @@
-function lookup(given_key, tree_of_records) {
-  return is_null(tree_of_records)
-    ? false
-    : given_key === key(entry(tree_of_records))
-    ? entry(tree_of_records)
-    : given_key < key(entry(tree_of_records))
-    ? lookup(given_key, left_branch(tree_of_records))
-    : // given_key > key(head(set_of_records))
-      lookup(given_key, right_branch(tree_of_records));
+import { is_null } from "sicp";
+
+function lookup(given_key, set_of_records) {
+  if (is_null(set_of_records)) {
+    return false;
+  } else {
+    const record = entry(set_of_records);
+    const record_key = key(record);
+
+    return given_key === record_key
+      ? record
+      : given_key < record_key
+      ? lookup(given_key, left_branch(set_of_records))
+      : // given_key > record_key
+        lookup(given_key, right_branch(set_of_records));
+  }
 }
