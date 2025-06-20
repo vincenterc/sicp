cd88f456cf2c696b55eddd9dd883db636ef90838
vincenter <vincenterr@yahoo.com.tw>
2025-06-20 09:48:30 +0800

    Add a table library

---
+++ solutions/table.test.js
@@ -0,0 +1,19 @@
+import { describe, expect, it } from "vitest";
+import { make_2d_table } from "./table.js";
+
+describe("the table library tests", () => {
+  it("tests the make_2d_table function", () => {
+    const table = make_2d_table();
+    const get = table("lookup");
+    const put = table("insert");
+
+    put("math", "+", 43);
+    put("math", "-", 45);
+    put("math", "*", 42);
+    put("letters", "a", 97);
+    put("letters", "b", 98);
+
+    expect(get("math", "-")).toBe(45);
+    expect(get("letters", "a")).toBe(97);
+  });
+});
