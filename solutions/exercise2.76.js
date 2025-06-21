2e7429d7bbd398554fdd88a98c158d465a3ed20d
vincenter <vincenterr@yahoo.com.tw>
2025-06-21 11:55:33 +0800

    Modify the exercise 2.76

--- solutions/exercise2.76.js
+++ solutions/exercise2.76.js
@@ -1,3 +1,22 @@
+// Generic operations with explicit dispatch:
+// - New type:
+//   * Add a new clause to each generic operation function
+// - New operation:
+//   * Add a new generic operation function
+// Data-directed style:
+// - New type:
+//   * Implement a new package for the new type and install it
+// - New operation:
+//   * Add a new operation to each package, or implement a new package
+//     that handles every type for the new operation and install it
+//   * Add a new generic operation function
+// Message-passing style:
+// - New type:
+//   * Add a new function that handles every operation
+// - New operation:
+//   * Add a new clause to each type's dispatch
+//   * Add a new generic operation function
+//
 // Message-passing style is appropriate for
 // a system in which new types must often be added, while
 // generic operations with explicit dispatch is appropriate for
