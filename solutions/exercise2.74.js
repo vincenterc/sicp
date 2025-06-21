01362e359c2cd0eb329cb372092620206bc07864
vincenter <vincenterr@yahoo.com.tw>
2025-06-21 10:20:49 +0800

    Modify exercise 2.74

--- solutions/exercise2.74.js
+++ solutions/exercise2.74.js
@@ -1,49 +1,69 @@
-import { accumulate, head, is_undefined, pair, tail } from "sicp";
+import {
+  accumulate,
+  error,
+  head,
+  is_null,
+  is_undefined,
+  list,
+  pair,
+  tail,
+} from "sicp";
+import { make_2d_table } from "./table.js";
 
 // a.
-// The division's files should be list(division_name, employee_records).
+// The division's files should be pair(division_name, employee_records).
 // Each division should also implement their own get_record and
 // install it.
 function make_division_file(division, records) {
   return pair(division, records);
 }
 
-function get_division_from_file(file) {
+function division_from_file(file) {
   return head(file);
 }
 
-function get_records_from_file(file) {
-  return head(tail(file));
+function records_from_file(file) {
+  return tail(file);
 }
 
 function get_record(file, employee) {
-  const division = get_division_from_file(file);
-  const records = get_records_from_file(file);
-  const record = get("get_record", division)(records, employee);
+  const division = division_from_file(file);
+  const records = records_from_file(file);
+  const get_record_division = get("get_record", division);
+  if (is_undefined(get_record_division)) {
+    return error(division, "No division's get_record -- get_record");
+  } else {
+    const record = get_record_division(records, employee);
 
-  return !is_undefined(record) ? attach_tag(division, record) : undefined;
+    return !is_undefined(record) ? attach_tag(division, record) : undefined;
+  }
 }
 
 // b.
-// The record should be list(division_name, record).
+// The record should be pair(division_name, record).
 // Each division should also implement their own get_salary and
 // install it.
-function get_division_from_record(record) {
+function division_from_record(record) {
   return head(record);
 }
 
-function get_content_from_record(record) {
-  return head(tail(record));
+function content_from_record(record) {
+  return tail(record);
 }
 
 function get_salary(record) {
   if (is_undefined(record)) {
     return undefined;
   } else {
-    const division = get_division_from_record(record);
-    const content = get_content_from_record(record);
+    const division = division_from_record(record);
+    const content = content_from_record(record);
+    const get_salary_division = get("get_salary", division);
 
-    return get("get_salary", division)(content);
+    if (is_undefined(get_salary_division)) {
+      return error(division, "No division's get_salary -- get_salary");
+    } else {
+      return get_salary_division(content);
+    }
   }
 }
 
@@ -60,3 +80,101 @@ function find_employee_record(employee, files) {
 // They should tag the new company's division files, implement
 // division-specific functions such as get_record and get_salary,
 // and install these functions.
+
+const table = make_2d_table();
+const get = table("lookup");
+const put = table("insert");
+const marketing = attach_tag(
+  "marketing",
+  list(
+    pair(
+      "James",
+      list(
+        pair("name", "James"),
+        pair("salary", 200),
+        pair("address", "Shinjuku, Tokyo")
+      )
+    ),
+    pair(
+      "Emily",
+      list(
+        pair("name", "Emily"),
+        pair("salary", 250),
+        pair("address", "Setagaya, Tokyo")
+      )
+    )
+  )
+);
+const sales = attach_tag(
+  "sales",
+  list(
+    pair(
+      "Sarah",
+      list(pair("salary", 220), pair("address", "Suginami, Tokyo"))
+    ),
+    pair("Emma", list(pair("salary", 260), pair("address", "Shibuya, Tokyo")))
+  )
+);
+const files = list(marketing, sales);
+
+install_marketing_package();
+install_sales_package();
+
+function install_marketing_package() {
+  function get_record(records, employee) {
+    return is_null(records)
+      ? undefined
+      : head(head(records)) === employee
+      ? tail(head(records))
+      : get_record(tail(records), employee);
+  }
+
+  function get_salary(record) {
+    return is_undefined(record)
+      ? undefined
+      : head(head(record)) === "salary"
+      ? tail(head(record))
+      : get_salary(tail(record));
+  }
+
+  put("get_record", "marketing", get_record);
+  put("get_salary", "marketing", get_salary);
+}
+
+function install_sales_package() {
+  function get_record(records, employee) {
+    return is_null(records)
+      ? undefined
+      : head(head(records)) === employee
+      ? head(records)
+      : get_record(tail(records), employee);
+  }
+
+  function get_salary(record) {
+    function iter(content) {
+      return is_undefined(content)
+        ? undefined
+        : head(head(content)) === "salary"
+        ? tail(head(content))
+        : get_salary(tail(content));
+    }
+
+    return is_undefined(record) ? undefined : iter(tail(record));
+  }
+
+  put("get_record", "sales", get_record);
+  put("get_salary", "sales", get_salary);
+}
+
+function attach_tag(type_tag, contents) {
+  return pair(type_tag, contents);
+}
+
+export {
+  marketing,
+  sales,
+  files,
+  get_record,
+  get_salary,
+  find_employee_record,
+};
---
+++ solutions/exercise2.74.test.js
@@ -0,0 +1,64 @@
+import { describe, expect, it } from "vitest";
+import { list, pair } from "sicp";
+import {
+  files,
+  find_employee_record,
+  get_record,
+  get_salary,
+  marketing,
+  sales,
+} from "./exercise2.74.js";
+
+describe("exercise 2.74", () => {
+  it("tests the get_record function", () => {
+    expect(get_record(marketing, "Emily")).toEqual(
+      pair(
+        "marketing",
+        list(
+          pair("name", "Emily"),
+          pair("salary", 250),
+          pair("address", "Setagaya, Tokyo")
+        )
+      )
+    );
+    expect(get_record(marketing, "Emma")).toBe(undefined);
+    expect(get_record(sales, "Emma")).toEqual(
+      pair(
+        "sales",
+        pair(
+          "Emma",
+          list(pair("salary", 260), pair("address", "Shibuya, Tokyo"))
+        )
+      )
+    );
+  });
+
+  it("tests the get_salary function", () => {
+    expect(get_salary(get_record(marketing, "James"))).toBe(200);
+    expect(get_salary(get_record(marketing, "Emma"))).toBe(undefined);
+    expect(get_salary(get_record(sales, "Sarah"))).toBe(220);
+  });
+
+  it("tests the find_employee_record function", () => {
+    expect(find_employee_record("James", files)).toEqual(
+      pair(
+        "marketing",
+        list(
+          pair("name", "James"),
+          pair("salary", 200),
+          pair("address", "Shinjuku, Tokyo")
+        )
+      )
+    );
+    expect(find_employee_record("David", files)).toBe(undefined);
+    expect(find_employee_record("Sarah", files)).toEqual(
+      pair(
+        "sales",
+        pair(
+          "Sarah",
+          list(pair("salary", 220), pair("address", "Suginami, Tokyo"))
+        )
+      )
+    );
+  });
+});
