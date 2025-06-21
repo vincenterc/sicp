e324a555c90979cfe8c71559c08020dd84734cef
vincenter <vincenterr@yahoo.com.tw>
2025-06-21 22:40:29 +0800

    Modify exercise 2.77

--- solutions/exercise2.77.js
+++ solutions/exercise2.77.js
@@ -6,10 +6,14 @@
 // == math_sqrt(square(real_part(z_without_complex_rectangular_tag))
 //            + square(image_part(z_without_complex_rectangular_tag)))
 // == 5
-
+//
 // z
 // == pair("complex", pair("rectangular", pair(3, 4)))
 // z_without_complex_tag
 // == pair("rectangular", pair(3, 4))
-// z_without_complex_rectangular_ta
+// z_without_complex_rectangular_tag
 // == pair(3, 4)
+//
+// apply_generic is invoked twice. The first time, the magnitude function
+// in the complex package is dispatched; the second time, the magnitude
+// function in the rectangle package is dispatched.
