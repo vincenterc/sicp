5e063cb3f5342deb193a172c43cba41deaf1c4bb
vincenter <vincenterr@yahoo.com.tw>
2025-06-24 20:17:50 +0800

    Modify exercises 2.79 and 2.80

--- solutions/exercise2.79.test.js
+++ solutions/exercise2.79.test.js
@@ -9,25 +9,25 @@ import {
 
 describe("exercise 2.79", () => {
   it("tests the is_equal function", () => {
-    expect(
-      is_equal(make_javascript_number(3), make_javascript_number(3)),
-    ).toBeTruthy();
-    expect(
-      is_equal(make_javascript_number(3), make_javascript_number(4)),
-    ).toBeFalsy();
-    expect(is_equal(make_rational(2, 3), make_rational(4, 6))).toBeTruthy();
-    expect(is_equal(make_rational(2, 5), make_rational(4, 6))).toBeFalsy();
+    expect(is_equal(make_javascript_number(3), make_javascript_number(3))).toBe(
+      true,
+    );
+    expect(is_equal(make_javascript_number(3), make_javascript_number(4))).toBe(
+      false,
+    );
+    expect(is_equal(make_rational(2, 3), make_rational(4, 6))).toBe(true);
+    expect(is_equal(make_rational(2, 5), make_rational(4, 6))).toBe(false);
     expect(
       is_equal(
         make_complex_from_real_imag(1, 0),
         make_complex_from_mag_ang(1, 0),
       ),
-    ).toBeTruthy();
+    ).toBe(true);
     expect(
       is_equal(
         make_complex_from_real_imag(1, 1),
         make_complex_from_mag_ang(1, 0),
       ),
-    ).toBeFalsy();
+    ).toBe(false);
   });
 });
--- solutions/exercise2.80.test.js
+++ solutions/exercise2.80.test.js
@@ -10,15 +10,13 @@ import { math_PI } from "sicp";
 
 describe("exercise 2.80", () => {
   it("tests the is_equal_to_zero function", () => {
-    expect(is_equal_to_zero(make_javascript_number(0))).toBeTruthy();
-    expect(is_equal_to_zero(make_javascript_number(1))).toBeFalsy();
-    expect(is_equal_to_zero(make_rational(0, 2))).toBeTruthy();
-    expect(is_equal_to_zero(make_rational(1, 2))).toBeFalsy();
-    expect(is_equal_to_zero(make_complex_from_real_imag(0, 0))).toBeTruthy();
-    expect(is_equal_to_zero(make_complex_from_real_imag(3, 4))).toBeFalsy();
-    expect(
-      is_equal_to_zero(make_complex_from_mag_ang(0, math_PI)),
-    ).toBeTruthy();
-    expect(is_equal_to_zero(make_complex_from_mag_ang(5, math_PI))).toBeFalsy();
+    expect(is_equal_to_zero(make_javascript_number(0))).toBe(true);
+    expect(is_equal_to_zero(make_javascript_number(1))).toBe(false);
+    expect(is_equal_to_zero(make_rational(0, 2))).toBe(true);
+    expect(is_equal_to_zero(make_rational(1, 2))).toBe(false);
+    expect(is_equal_to_zero(make_complex_from_real_imag(0, 0))).toBe(true);
+    expect(is_equal_to_zero(make_complex_from_real_imag(3, 4))).toBe(false);
+    expect(is_equal_to_zero(make_complex_from_mag_ang(0, math_PI))).toBe(true);
+    expect(is_equal_to_zero(make_complex_from_mag_ang(5, math_PI))).toBe(false);
   });
 });
